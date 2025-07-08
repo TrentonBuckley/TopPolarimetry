e673735c84641b50c305e195b1c6732822dbc953
lvaughan <luke.vaughan@okstate.edu>
2025-07-08 14:14:39 -0500

    add top to vectors

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 # Load custom functions from another file
-from helper_functions import load_data, plot_hists, initialize_vectors, plot_pT_from_vectors
+from helper_functions import *
 
 # The main function
 def main():
     # The path to the left handed file
     path_L="hard_process_L_10k.root"
-    # Extract the data
     E, px, py, pz, pid = load_data(path_L)
-    # Plot the histograms
-    plot_hists(E, px, py, pz, pid)
-    # Load the data into vectors
-    vectors = initialize_vectors(E, px, py, pz, pid)
-    # Plot pT using the vector list
-    plot_pT_from_vectors(vectors)
+
+    E_top, px_top, py_top, pz_top = select_top(E, px, py, pz, pid)
+    plot_hists("Top E", E_top)
+    top_vectors = initialize_vectors(E_top, px_top, py_top, pz_top, pid)
+    plot_mass_from_vectors("Top Mass", top_vectors)
+
+    #E_down, px_down, py_down, pz_down = select_down_type(E, px, py, pz, pid)
+    #plot_hists("Down E", E_down)
+    #down_vectors = initialize_vectors(E_down, px_down, py_down, pz_down, pid)
+    #plot_mass_from_vectors("Down Type Mass", down_vectors)
 
 # Run this script using "python main.py" in the terminal
 if __name__ == "__main__":
